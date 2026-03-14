389a5174230214d17e967c2fbd12c6ed47f87455
thong35 <thongks789@gmail.com>
2026-03-14 21:55:59 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+music.play(music.createSoundExpression(
+WaveShape.Sine,
+0,
+5000,
+255,
+0,
+3000,
+SoundExpressionEffect.None,
+InterpolationCurve.Linear
+), music.PlaybackMode.UntilDone)
 basic.forever(function () {
     basic.showString("Hello!")
 })
